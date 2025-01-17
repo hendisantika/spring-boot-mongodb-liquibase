f9a43c5410f1823b055118bf50694bb3de684622
hendisantika <hendisantika@yahoo.co.id>
2025-01-18 06:15:06 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/java/id/my/hendisantika/springbootmongodbliquibase/SpringBootMongodbLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootmongodbliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbLiquibaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
