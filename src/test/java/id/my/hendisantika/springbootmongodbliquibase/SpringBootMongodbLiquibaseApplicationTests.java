8f01010d87f1aad60ead4a258aa59221092f977f
hendisantika <hendisantika@yahoo.co.id>
2025-01-18 06:56:37 +0700

    fix: MongoDB TestContainer🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/springbootmongodbliquibase/SpringBootMongodbLiquibaseApplicationTests.java
+++ src/test/java/id/my/hendisantika/springbootmongodbliquibase/SpringBootMongodbLiquibaseApplicationTests.java
@@ -1,13 +1,29 @@
 package id.my.hendisantika.springbootmongodbliquibase;
 
-import org.junit.jupiter.api.Test;
+import id.my.hendisantika.springbootmongodbliquibase.customer.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@Testcontainers
+@SpringBootTest(
+        properties = {
+                "management.endpoint.health.show-details=always",
+                "spring.datasource.url=jdbc:tc:mongodb:8.0.4:///customerDB"
+        },
+        webEnvironment = RANDOM_PORT
+)
 class SpringBootMongodbLiquibaseApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @BeforeEach
+    void deleteAll() {
+        customerRepository.deleteAll();
     }
 
 }
